cac9309c2db70e4c4373ce959f5011f3dd9cf160
Fkids <contact@fkids.net>
2016-07-15 15:32:15 +0300

    Made local storage require consent from the sign up

--- client/modules/Local.js
+++ client/modules/Local.js
@@ -3,6 +3,7 @@ var Local = {}
 // Retrieve local storage value
 Local.get = function(name, defaultValue)
 {
+	// Local storage not supported, bail out with default value
 	if(typeof(Storage) == "undefined")
 		return defaultValue;
 
@@ -17,8 +18,18 @@ Local.get = function(name, defaultValue)
 // Store local storage value
 Local.set = function(name, value)
 {
+	// Local storage not supported, bail out
 	if(typeof(Storage) == "undefined")
 		return;
+
+	var Account = require("./Account.js");
+
+	// Don't store anything when not authenticated, unless the
+	// storage already has items in it, which would mean the consent
+	// to use local storage has been provided upon sign up
+	if( !Account.authenticated &&
+		localStorage.length == 0 )
+			return;
 
 	localStorage.setItem(name, value);
 }
